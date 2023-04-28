a9140c4d047aec6af11fe56e287b56603f87d37c
Rafael Pereira de Lima <rafael_pereira_007@hotmail.com>
2023-04-28 16:37:55 -0300

    Swagger

--- src/main/java/br/com/example/ExampleApplication.java
+++ src/main/java/br/com/example/ExampleApplication.java
@@ -1,5 +1,8 @@
 package br.com.example;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -8,6 +11,10 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @EnableAsync
 @EntityScan(basePackages = {"br.com.example.model.dao"})
+@OpenAPIDefinition(
+		info = @Info(title = "Api de Pessoas", version = "3.0.1", description = "Api"),
+		servers = {@Server(url = "http://localhost:8080")}
+)
 public class ExampleApplication {
 
 	public static void main(String[] args) {
